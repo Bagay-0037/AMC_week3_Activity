967bae338cc77e5d0b32ece99b2c8a9a4c9b6745
Bagay <137521953+Bagay-0037@users.noreply.github.com>
2025-01-27 12:44:42 +0800

    FlexDirection

---
+++ Activity 3/Row/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const Flex = () => {
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          // Try setting `flexDirection` to `"row"`.
+          flexDirection: 'row',
+        },
+      ]}>
+      <View style={[styles.box, {backgroundColor: 'powderblue'}]} />
+      <View style={[styles.box, {backgroundColor: 'skyblue'}]} />
+      <View style={[styles.box, {backgroundColor: 'steelblue'}]} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  box: {
+    width: 50,
+    height: 50,
+  },
+});
+
+export default Flex;
