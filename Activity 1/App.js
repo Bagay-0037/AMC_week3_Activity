f2c1f075ae12a5869799b5bed91afe163915bbc2
Bagay <137521953+Bagay-0037@users.noreply.github.com>
2025-01-27 12:33:55 +0800

    TextInput

---
+++ Activity 1/App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {StyleSheet, TextInput} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+const TextInputExample = () => {
+  const [text, onChangeText] = React.useState('NAME: ');
+  const [age, onChangeText2] = React.useState('AGE: ');
+  const [address, onChangeText3] = React.useState('ADDRESS: ');
+  const [school, onChangeText4] = React.useState('SCHOOL: ');
+  const [course, onChangeText5] = React.useState('COURSE: ');
+  const [email, onChangeText6] = React.useState('EMAIL: ');
+  const [cNum, onChangeText7] = React.useState('CONTACT NO: ');
+  return(
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <TextInput
+          style = {styles.input}
+          onChangeText = {onChangeText}
+          value = {text}
+        />
+        <TextInput
+          style = {styles.input}
+          onChangeText = {onChangeText2}
+          value = {age}
+        />
+        <TextInput
+          style = {styles.input}
+          onChangeText = {onChangeText3}
+          value = {address}
+        />
+        <TextInput
+          style = {styles.input}
+          onChangeText = {onChangeText4}
+          value = {school}
+        />
+        <TextInput
+          style = {styles.input}
+          onChangeText = {onChangeText5}
+          value = {course}
+        />
+        <TextInput
+          style = {styles.input}
+          onChangeText = {onChangeText6}
+          value = {email}
+        />
+        <TextInput
+          style = {styles.input}
+          onChangeText = {onChangeText7}
+          value = {cNum}
+        />
+        
+      </SafeAreaView>
+    </SafeAreaProvider>
+    
+  );
+};
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+export default TextInputExample;
